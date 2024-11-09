9cb779f0021ae8df031de4d6dda7717054cee529
FHWz5484728 <z5484728@ad.unsw.edu.au.com>
2024-11-09 16:43:05 +1100

    basic structure of login and regist

---
+++ frontend/src/components/LoginForm.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import {
+    BrowserRouter,
+    Route,Routes,
+  } from "react-router-dom";
+
+const LoginForm=()=>{
+    return (
+        <>login page</>
+    )
+}
+
+export {LoginForm};
---
+++ frontend/src/components/RegisterForm.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import {
+    BrowserRouter,
+    Route,Routes,
+  } from "react-router-dom";
+
+const RegisterForm=()=>{
+    return (
+        <>Register page</>
+    )
+}
+
+export {RegisterForm};
