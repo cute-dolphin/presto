bd9d79d9ce0ff679e6b4b5bc75827e45e0d6961f
FHWz5484728 <z5484728@ad.unsw.edu.au.com>
2024-11-09 20:20:04 +1100

    basic UI of LoginPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg';
 import {BrowserRouter,Route,Routes,} from "react-router-dom";
 import { LandingPage } from './components/LandingPage';
 import { RegisterForm } from './components/RegisterForm';
+import { LoginPage } from './components/LoginPage';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path='/' element={<LandingPage/>}></Route>
           <Route path='/Register' element={<RegisterForm/>}></Route>
+          <Route path='/Login' element={<LoginPage/>}></Route>
         </Routes>
       </BrowserRouter>
     </>
--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -3,10 +3,24 @@ import {
     BrowserRouter,
     Route,Routes,
   } from "react-router-dom";
+  import Box from '@mui/material/Box';
+  import TextField from '@mui/material/TextField';
+  import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
 const LoginPage=()=>{
     return (
-        <div>LoginPage</div>
+        <>
+            <div>
+                <TextField id="outlined-basic" label="Email:" variant="outlined" />
+            </div>
+            <div>
+                <TextField id="outlined-basic" label="Password:" variant="outlined" />
+            </div>
+            <div>
+                <Button variant="outlined">Submit</Button>
+            </div>
+        </>
     )
 }
 
