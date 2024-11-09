77cd8c57de48c3ddf26f4589b6c3c82ace0b711d
FHWz5484728 <z5484728@ad.unsw.edu.au.com>
2024-11-09 19:58:47 +1100

    unstall material-UI

---
+++ frontend/src/components/LoginPage.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import {
+    BrowserRouter,
+    Route,Routes,
+  } from "react-router-dom";
+
+const LoginPage=()=>{
+    return (
+        <div>LoginPage</div>
+    )
+}
+
+export {LoginPage};
