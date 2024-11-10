648e064f11acd3000ed5dd8f8b3d14397c0c9d9c
FHWz5484728 <z5484728@ad.unsw.edu.au.com>
2024-11-10 14:45:08 +1100

    LandingPage format

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -1,8 +1,12 @@
 import { useState } from 'react';
-import {BrowserRouter,Route,Routes,} from "react-router-dom";
+import {BrowserRouter,Route,Routes,Link} from "react-router-dom";
 const LandingPage=()=>{
     return (
-        <>Landing page</>
+        <>
+            <div>Welcome to Landing Page</div>
+            <Link to='/login'>Login</Link><>||</>
+            <Link to='/register'>Register</Link>
+        </>
     )
 }
 
