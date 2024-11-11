8e4a9ec34e38d1940147b0a9574fa71a0c06fecb
FHWz5484728 <z5484728@ad.unsw.edu.au.com>
2024-11-11 19:42:16 +1100

    2.2.2 show pre complete

--- frontend/src/components/MediaCard.jsx
+++ frontend/src/components/MediaCard.jsx
@@ -5,29 +5,36 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-
+//2.2.2 show all presentation
 export const MediaCard=(props)=> {
+  const allPre=props.presentation ? Object.entries(props.presentation) : [];
+  console.log(allPre);
+  if(allPre.length===0){
+    return null;
+  }
+  //if exist pre, show
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        sx={{ height: 140 }}
-        image="/static/images/cards/contemplative-reptile.jpg"
-        title="green iguana"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Lizard
-        </Typography>
-        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+    <div>
+        {allPre.map(([id,presentation])=>(
+            <Card key={id} sx={{ maxWidth: 345 }}>
+            <CardMedia
+                sx={{ height: 140 }}
+                image="/static/images/cards/contemplative-reptile.jpg"
+                title="green iguana"
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                {presentation.title}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small">Share</Button>
+                <Button size="small">Learn More</Button>
+            </CardActions>
+            </Card>
+        ))}
+    </div>
+    
   );
 }
 export default MediaCard;
